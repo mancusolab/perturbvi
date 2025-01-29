fixed missing KL term that was removed during bug hunting. simplified computation of a few terms

src/perturbvi/factorloadings.py
@@ -30,7 +30,7 @@ class FactorModel(eqx.Module):
 
     def update(self, data: DataMatrix, guide: GuideModel, loadings: "LoadingModel", params: ModelParams) -> ModelParams:
         mean_w, mean_ww = loadings.moments(params)
-        n_dim, z_dim = self.shape
+        _, z_dim = self.shape
 
         update_var_z = jnp.linalg.inv(params.tau * mean_ww + jnp.identity(z_dim))
         update_mu_z = (params.tau * (data @ mean_w.T) + guide.predict(params)) @ update_var_z
@@ -41,7 +41,7 @@ class FactorModel(eqx.Module):
         )
 
     def moments(self, params: ModelParams) -> FactorMoments:
-        n_dim, z_dim = self.shape
+        n_dim, _ = self.shape
 
         # compute expected residuals
         # use posterior mean of Z, W, and Alpha to calculate residuals
@@ -62,13 +62,13 @@ class FactorModel(eqx.Module):
         #  = sum(mean_z ** 2) + n * tr(var_z)
         # NB: tr(E_q[Z]' M E_prior[Z]) = sum(E_q[Z] * (M E_prior[Z])); saves factor of n
         # guide.weighted_sumsq(params) = tr(M'E[BB']M); can change depending on guide model
-        t1 =    jnp.sum(mean_z**2)
-        t2 =    n_dim * jnp.trace(var_z)
-        t3 =    - 2 * jnp.sum(mean_z * pred_z)
-        t4 =    guide.weighted_sumsq(params)
-        t5 =  -n_dim * z_dim
-        t6 =   - n_dim * logdet(params.var_z)
-        kl_d_ = 0.5 * (t1 + t2 + t3 + t4 + t5)
+        t1 = jnp.sum(mean_z**2)
+        t2 = n_dim * jnp.trace(var_z)
+        t3 = -2 * jnp.sum(mean_z * pred_z)
+        t4 = guide.weighted_sumsq(params)
+        t5 = -n_dim * z_dim
+        t6 = -n_dim * logdet(params.var_z)
+        kl_d_ = 0.5 * (t1 + t2 + t3 + t4 + t5 + t6)
         return kl_d_
 
 
@@ -134,7 +134,7 @@ class _FactorLoopResults(NamedTuple):
 
 def _loop_factors(kdx: int, loop_params: _FactorLoopResults) -> _FactorLoopResults:
     data, W, mean_zz, params = loop_params
-    l_dim, z_dim, p_dim = params.mean_w.shape
+    l_dim, z_dim, _ = params.mean_w.shape
 
     # sufficient stats for inferring downstream w_kl/alpha_kl
     not_kdx = jnp.where(jnp.arange(z_dim) != kdx, size=z_dim - 1)
@@ -166,9 +166,9 @@ class LoadingModel(eqx.Module):
         return self.l_dim, self.z_dim, self.p_dim
 
     def update(self, data: DataMatrix, factors: FactorModel, params: ModelParams) -> ModelParams:
-        l_dim, z_dim, p_dim = self.shape
-        mean_z, mean_zz = factors.moments(params)
-        mean_w, mean_ww = self.moments(params)
+        _, z_dim, _ = self.shape
+        _, mean_zz = factors.moments(params)
+        mean_w, _ = self.moments(params)
 
         # update locals (W, alpha)
         init_loop_param = _FactorLoopResults(data, mean_w, mean_zz, params)
@@ -184,10 +184,7 @@ class LoadingModel(eqx.Module):
         return params._replace(tau_0=u_tau_0)
 
     def moments(self, params: ModelParams) -> LoadingMoments:
-        trace_var = jnp.sum(
-            params.var_w[:, :, jnp.newaxis] * params.alpha + (params.mean_w**2 * params.alpha * (1 - params.alpha)),
-            axis=(-1, 0),
-        )
+        trace_var = jnp.sum(params.var_w[:, :, jnp.newaxis] * params.alpha, axis=(-1, 0))
         mu_w = jnp.sum(params.mean_w * params.alpha, axis=0)
         moments_ = LoadingMoments(
             mean_w=mu_w,

src/perturbvi/guide.py
@@ -27,16 +27,17 @@ def _get_diag(G: Array) -> Array:
 def _get_diag(G: SparseMatrix) -> Array:
     return jsparse.sparsify(jnp.sum)(G.matrix**2, axis=0).todense()  # type: ignore
 
+
 @dispatch
 def _wgt_sumsq(G: SparseMatrix, vector: Array) -> Array:
     tmp = G.matrix * vector
-    return jsparse.sparsify(jnp.sum)(tmp**2)
+    return jsparse.sparsify(jnp.sum)(tmp**2)  # type: ignore
 
 
 @dispatch
 def _wgt_sumsq(G: Array, vector: Array) -> Array:
     tmp = G * vector
-    return jnp.sum(tmp ** 2)
+    return jnp.sum(tmp**2)
 
 
 _multi_linear_solve = eqx.filter_vmap(lx.linear_solve, in_axes=(None, 1, None))
@@ -81,26 +82,21 @@ class GuideModel(eqx.Module):
         return self.guide_data.shape
 
     @abstractmethod
-    def weighted_sumsq(self, params: ModelParams) -> Array:
-        ...
+    def weighted_sumsq(self, params: ModelParams) -> Array: ...
 
     @abstractmethod
-    def predict(self, params: ModelParams) -> Array:
-        ...
+    def predict(self, params: ModelParams) -> Array: ...
 
     @abstractmethod
-    def update(self, params: ModelParams) -> ModelParams:
-        ...
+    def update(self, params: ModelParams) -> ModelParams: ...
 
     @staticmethod
     @abstractmethod
-    def update_hyperparam(params: ModelParams) -> ModelParams:
-        ...
+    def update_hyperparam(params: ModelParams) -> ModelParams: ...
 
     @staticmethod
     @abstractmethod
-    def kl_divergence(params: ModelParams) -> Array:
-        ...
+    def kl_divergence(params: ModelParams) -> Array: ...
 
 
 class SparseGuideModel(GuideModel):
@@ -116,7 +112,7 @@ class SparseGuideModel(GuideModel):
         # however notice that only intercept in non-zero in GtG off-diag
         # ZkG = params.mean_z[:,kdx].T @ G - GtG_diag * params.B[-1,kdx]
         # without intercept
-        # ZkG = params.mean_z.T @ self.guide_data 
+        # ZkG = params.mean_z.T @ self.guide_data
 
         # capture global mean per latent variable to adjust both target/non-target cells for avg effect
         inter = jnp.mean(params.mean_z, axis=0)
@@ -181,4 +177,3 @@ class DenseGuideModel(GuideModel):
     @staticmethod
     def kl_divergence(params: ModelParams) -> Array:
         return jnp.asarray(0.0)
-

src/perturbvi/infer.py
@@ -35,8 +35,8 @@ def _is_valid(X: sparse.JAXSparse):
 
 
 def _update_tau(X: DataMatrix, factor: FactorModel, loadings: LoadingModel, params: ModelParams) -> ModelParams:
-    n_dim, z_dim = params.mean_z.shape
-    l_dim, z_dim, p_dim = params.mean_w.shape
+    n_dim, _ = params.mean_z.shape
+    _, _, p_dim = params.mean_w.shape
 
     # calculate moments of factors and loadings
     mean_z, mean_zz = factor.moments(params)
@@ -59,7 +59,7 @@ class ELBOResults(NamedTuple):
         kl_factors: -KL divergence of Z
         kl_loadings: -KL divergence of W
         kl_guide: -KL divergence of B
-    
+
     """
 
     elbo: Array
@@ -100,8 +100,8 @@ def compute_elbo(
     - `ELBOResults` [`ELBOResults`]: The object contains all components in ELBO
 
     """
-    n_dim, z_dim = params.mean_z.shape
-    l_dim, z_dim, p_dim = params.mean_w.shape
+    n_dim, _ = params.mean_z.shape
+    _, _, p_dim = params.mean_w.shape
 
     # calculate second moment of Z along k, (k x k) matrix
     # E[Z'Z] = V_k[Z] * tr(I_n) + E[Z]'E[Z] = V_k[Z] * n + E[Z]'E[Z]
@@ -111,10 +111,11 @@ def compute_elbo(
     mean_w, mean_ww = loadings.moments(params)
 
     # expectation of log likelihood
-    # calculation tip: tr(A @ A.T) = tr(A.T @ A) = sum(A ** 2)
+    # calculation tip: tr(A @ A.T) = tr(A.T @ A) = sum(A ** 2); but doubles mem
+    # tr(A.T @ A) is inner product of A with itself = vdot(X, X)
     # (X.T @ E[Z] @ E[W]) is p x p (big!); compute (E[W] @ X.T @ E[Z]) (k x k)
     exp_logl = (-0.5 * params.tau) * (
-        jnp.sum(X**2)
+        jnp.vdot(X, X)
         - 2 * jnp.einsum("kp,np,nk->", mean_w, X, mean_z)  # tr(E[W] @ X.T @ E[Z])
         + jnp.einsum("ij,ji->", mean_zz, mean_ww)  # tr(E[Z.T @ Z] @ E[W @ W.T])
     ) + 0.5 * n_dim * p_dim * jnp.log(params.tau)
@@ -267,9 +268,8 @@ def _init_params(
         alpha_key,
         beta_key,
         var_beta_key,
-        p_key,
         theta_key,
-    ) = random.split(rng_key, 11)
+    ) = random.split(rng_key, 10)
 
     # pull type options for init
     type_options = get_args(_init_type)
@@ -441,7 +441,8 @@ def infer(
     -`A` [`Array`]: Annotation matrix to use in parameterized-prior mode. If not `None`, leading dimension
         should match the feature dimension of X.
 
-    -`p_prior` [`float`]: Prior probability for each perturbation to have a non-zero effect to predict latent factor. (default = 0.5)
+    -`p_prior` [`float`]: Prior probability for each perturbation to have a non-zero effect to predict latent factor.
+        (default = 0.5)
 
     -`tau` [`float`]: initial value of residual precision (default = 1)
 
@@ -463,7 +464,7 @@ def infer(
 
     **Returns:**
 
-    -`InferResults`: The dictionary contain all the infered parameters.
+    An [`InferResults`][] object  contain all the infered parameters.
     """
 
     # sanity check arguments
@@ -539,7 +540,7 @@ def compute_pip(params: ModelParams) -> Array:
 
     **Returns:**
 
-    -`PIP` [`Array`]: Array of posterior inclusion probabilities (PIPs) for each of `K x P` factor, 
+    -`PIP` [`Array`]: Array of posterior inclusion probabilities (PIPs) for each of `K x P` factor,
               feature combinations
 
     """

src/perturbvi/sim.py
@@ -72,7 +72,7 @@ def generate_sim(
     b_sparsity: float = 0.2,
     effect_size: float = 1.0,
 ) -> SimulatedData:
-    """Create the function to generate a sparse data for perturbVI. 
+    """Create the function to generate a sparse data for perturbVI.
        Please make sure l_dim < p_dim/z_dim
        when generate simulation data using this function.
 
@@ -153,7 +153,6 @@ def generate_sim(
     return SimulatedData(Z, W, X, G, beta)
 
 
-
 def generate_sim_with_control(
     seed: int,
     l_dim: int,
@@ -162,10 +161,10 @@ def generate_sim_with_control(
     z_dim: int,
     g_dim: int,
     control_fraction: float = 0.2,
-    b_sparsity: float = 0.2, 
+    b_sparsity: float = 0.2,
     effect_size: float = 1.0,
 ) -> SimulatedData:
-    """Create the function to generate a sparse data for perturbVI. 
+    """Create the function to generate a sparse data for perturbVI.
        Please make sure l_dim < p_dim/z_dim
        when generate simulation data using this function.
 
@@ -176,7 +175,7 @@ def generate_sim_with_control(
         p_dim: Number of feature in the data
         z_dim: Number of Latent dimensions
         g_dim: perturbation dimensions
-        control_fraction: fraction of negative control guide 
+        control_fraction: fraction of negative control guide
         b_sparsity: sparsity of perturbation effects.
         effect_size: The effect size of features contributing to the factor.
                       (default = 1).
@@ -194,9 +193,7 @@ def generate_sim_with_control(
 
     # dimension check
     if l_dim > p_dim:
-        raise ValueError(
-            f"l_dim should be less than p: received l_dim = {l_dim}, p = {p_dim}"
-        )
+        raise ValueError(f"l_dim should be less than p: received l_dim = {l_dim}, p = {p_dim}")
     if l_dim > p_dim / z_dim:
         raise ValueError(
             f"""l_dim is smaller than p_dim/z_dim,
@@ -207,31 +204,23 @@ def generate_sim_with_control(
         raise ValueError(f"l_dim should be positive: received l_dim = {l_dim}")
 
     if z_dim > p_dim:
-        raise ValueError(
-            f"z_dim should be less than p: received z_dim = {z_dim}, p = {p_dim}"
-        )
+        raise ValueError(f"z_dim should be less than p: received z_dim = {z_dim}, p = {p_dim}")
     if z_dim > n_dim:
-        raise ValueError(
-            f"z_dim should be less than n: received z_dim = {z_dim}, n = {n_dim}"
-        )
+        raise ValueError(f"z_dim should be less than n: received z_dim = {z_dim}, n = {n_dim}")
     if z_dim <= 0:
         raise ValueError(f"z_dim should be positive: received z_dim = {z_dim}")
 
     if effect_size <= 0:
-        raise ValueError(
-            f"effect size should be positive: received effect_size = {effect_size}"
-        )
-    
-    #control data
+        raise ValueError(f"effect size should be positive: received effect_size = {effect_size}")
+
+    # control data
     control_size = int(n_dim * control_fraction)
 
     # random W
     W = jnp.zeros(shape=(z_dim, p_dim))
 
     for k in range(z_dim):
-        W = W.at[k, (k * l_dim) : ((k + 1) * l_dim)].set(
-            effect_size * random.normal(b_key, shape=(l_dim,))
-        )
+        W = W.at[k, (k * l_dim) : ((k + 1) * l_dim)].set(effect_size * random.normal(b_key, shape=(l_dim,)))
 
     # linear function to generate Z
     # perturbation effects
@@ -243,26 +232,24 @@ def generate_sim_with_control(
         beta_key, beta_key_2 = random.split(beta_key)
         # Step 2: Randomly select `non_zero_num` indices without replacement
         indices = random.choice(beta_key, g_dim, shape=(non_zero_num,), replace=False)
-            
+
         # Step 3: Generate `non_zero_num` random values from a normal distribution
         random_values = random.normal(beta_key_2, (non_zero_num,))
-            
+
         # Update the selected entries in the column with random values
         beta = beta.at[indices, col].set(random_values)
 
     # compose new G with control cell
     G_case = create_design_matrix(s_key, n_dim, g_dim)
     G_add_zero_col = jnp.insert(G_case, 0, values=0, axis=1)
-    G_control = jnp.hstack((jnp.zeros(shape = (control_size, g_dim)),jnp.ones(shape=(control_size,1))))
+    G_control = jnp.hstack((jnp.zeros(shape=(control_size, g_dim)), jnp.ones(shape=(control_size, 1))))
     G = jnp.vstack((G_add_zero_col, G_control))
-    beta_add = jnp.vstack((beta, 0.5*random.normal(beta_key_2, shape=(1, z_dim))))
-    #beta_add_zero_row = jnp.insert(beta, beta.shape[0], values=0, axis=0)
+    beta_add = jnp.vstack((beta, 0.5 * random.normal(beta_key_2, shape=(1, z_dim))))
+    # beta_add_zero_row = jnp.insert(beta, beta.shape[0], values=0, axis=0)
 
     Z = G @ beta_add + random.normal(var_key, shape=(n_dim + control_size, z_dim))
     # Latent factor model
     m = Z @ W
     X = m + random.normal(obs_key, shape=(n_dim + control_size, p_dim))
-    
+
     return SimulatedData(Z, W, X, G, beta_add)
-
-

src/perturbvi/utils.py
@@ -18,7 +18,7 @@ outer_add = _add_ufunc.outer
 
 
 def logdet(A: Array) -> Array:
-    sign, ldet = jnp.linalg.slogdet(A)
+    _, ldet = jnp.linalg.slogdet(A)
     return ldet
 
 
@@ -71,14 +71,14 @@ def prob_pca(rng_key, X, k, max_iter=1000, tol=1e-3):
 
     # check if reach the max_iter, or met the norm criterion every 100 iteration
     def _condition(carry):
-        i, W, Z, old_Z = carry
+        i, _, Z, old_Z = carry
         iter_check = i < max_iter
         tol_check = jnp.linalg.norm(Z - old_Z) > tol
         return iter_check & tol_check
 
     # EM algorithm for PPCA
     def _step(carry):
-        i, W, Z, old_Z = carry
+        i, W, Z, _ = carry
 
         # E step
         W_op = lx.MatrixLinearOperator(W @ W.T, tags=lx.positive_semidefinite_tag)
@@ -115,7 +115,7 @@ def bern_sample(alpha):
     - `efficient_result_matrix` [`Array`]: The sampled matrix.
 
     """
-    l_dim, z_dim, p_dim = alpha.shape
+    l_dim, z_dim, _ = alpha.shape
     # Generate random numbers for each row in the L x K matrix
     # These random numbers are used as indices for selecting features
     random_indices = np.random.rand(l_dim, z_dim)
@@ -148,7 +148,7 @@ def compute_lfsr(params, iters=2000):
     - `lfsr` [`Array`]: The LFSR for each of `L` single effects.
 
     """
-    l_dim, z_dim, p_dim = params.alpha.shape
+    _, _, p_dim = params.alpha.shape
     g_dim, _ = params.mean_beta.shape
     # Reshaping the var_w to (L by K) such that each value in var_w repeats P times
     reshaped_var_w = np.repeat(params.var_w[:, :, np.newaxis], p_dim, axis=2)
@@ -193,9 +193,7 @@ def pip_analysis(pip: jnp.ndarray, rho=0.9, rho_prime=0.05):
     for k in range(z_dim):
         num_signal = jnp.where(pip[k, :] >= rho)[0].shape[0]
         num_zero = jnp.where(pip[k, :] < rho_prime)[0].shape[0]
-        print(
-            f"Component {k} has {num_signal} features with pip>{rho}; " f"and {num_zero} features with pip<{rho_prime}"
-        )
+        print(f"Component {k} has {num_signal} features with pip>{rho}; and {num_zero} features with pip<{rho_prime}")
         results.append([num_signal, num_zero])
 
     df = pd.DataFrame(results, columns=["num_signal", "num_zero"])